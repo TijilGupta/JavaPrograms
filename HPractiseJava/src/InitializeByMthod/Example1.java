9684a8fdec4979a77acf5d1113717aff7ecbddc9
TijilGupta <61958303+TijilGupta@users.noreply.github.com>
2023-09-06 22:01:05 +0530

    Done some changes becoz program was excuting abnormallly.

--- HPractiseJava/src/InitializeByMthod/Example1.java
+++ HPractiseJava/src/InitializeByMthod/Example1.java
@@ -3,17 +3,23 @@ package InitializeByMthod;
 import InitilizeObjectByRefrenceVariable.Animal;
 
 public class Example1 {
-	public static void main(String[]args)
-	{
-		
+	
 		String colour;
 		int age;
+		void obj(String c,int a)
+		{
+			String color = c;
+			int age=a;
+		}
 		
-		void 
+		void display()   // i have done some changes becoz program was executing abnormally.
 		 {
 		System.out.println(colour+""+age);
 		 }
-		 Animal buzo=new Animal();
+		public static void main(String[]args)
+		{
+			
+		 Example1 buzo=new Example1();
 				buzo.obj("black",8);
 				buzo.display();
 		
